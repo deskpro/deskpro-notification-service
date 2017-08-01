8b41c0c2f0c55284dd56353a6ae081ffbb42d2e5
ImmortaL <denis@ranneft.ru>
2017-08-01 22:29:44 +0300

    use immutable for sockets, handle multiple tabs

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import express from 'express';
 import http from 'http';
 import SocketIO from 'socket.io';
 import bodyParser from 'body-parser';
+import Immutable from 'immutable';
 
 const app = express();
 const server = http.Server(app);
 const io = new SocketIO(server);
-const sockets = {};
+let sockets = Immutable.Map({});
+let socketsMap = Immutable.Map({});
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,9 +18,10 @@ app.use(bodyParser.json());
 
 app.post('/send', function(req, res){
   req.body.map((item) => {
-    if (sockets[item.channel]) {
-      const socket = sockets[item.channel];
-      socket.emit(item.name, item.data);
+    if (sockets.has(item.channel) && sockets.get(item.channel).length > 0) {
+      sockets.get(item.channel).map((socket) => {
+        socket.emit(item.name, item.data);
+      });
     }
   });
   res.end();
@@ -28,7 +31,10 @@ server.listen(3000);
 
 const authCallback = (socket, next) => (socketId) => {
   if(socketId) {
-    sockets[socketId] = socket;
+    const newSockets = sockets.has(socketId) ? sockets.get(socketId) : [];
+    newSockets.push(socket);
+    sockets = sockets.set(socketId, newSockets);
+    socketsMap = socketsMap.set(socket.id, socketId);
     return next();
   } else {
     return next(new Error('Auth failed'));
@@ -70,4 +76,14 @@ io.use((socket, next) => {
 
 io.on('connection', function (socket) {
   socket.emit('connected', { status: 200 });
+
+  socket.on('disconnect', function () {
+    if(socketsMap.has(socket.id)) {
+      const removeFrom = socketsMap.get(socket.id);
+      const newSockets = sockets.get(removeFrom).filter((item) => item.id !== socket.id);
+      sockets = sockets.set(removeFrom, newSockets);
+      socketsMap = socketsMap.delete(socket.id);
+    }
+  });
 });
+
