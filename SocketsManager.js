cc3b510c364905b8b961a10e0886b28f697b5c56
ImmortaL <denis@ranneft.ru>
2018-02-13 22:08:20 +0300

    updated how sockets are stored, updated how messages are emitted

--- SocketsManager.js
+++ SocketsManager.js
@@ -10,42 +10,54 @@ export default class SocketsManager
   
   sendMessage(message) {
     if (this.sockets.has(message.channel) && this.sockets.get(message.channel).length > 0) {
-      this.sockets.get(message.channel).map((socket) => {
-        if(this.debug) {
-          console.log(`
+      if(this.debug) {
+        console.log(`
 =======================
 New message via channel: ${message.channel}
 Message name: ${message.name}
 Data: ${JSON.stringify(message.data, null, 2)}
 =======================\r\n
           `);
-        }
+      }
 
-        socket.emit(message.name, message.data);
+      this.sockets.get(message.channel).map((socket) => {
+        socket.emit(`${message.channel}-${message.name}`, message.data);
       });
     }
   }
   
   addSocket(userId, socket) {
-    if(userId) {
-      const newSockets = this.sockets.has(userId) ? this.sockets.get(userId) : [];
-      newSockets.push(socket);
-      this.sockets = this.sockets.set(userId, newSockets);
-      this.socketsMap = this.socketsMap.set(socket.id, userId);
-      if(this.debug) {
-        console.log(`Socket with id ${socket.id} added for user #${userId}`);
-      }
+    let channel;
+    if(parseInt(userId, 10)) {
+      channel = `private-${userId}`
+    } else {
+      channel = userId;
+    }
+
+    const newSockets = this.sockets.has(channel) ? this.sockets.get(channel) : [];
+    newSockets.push(socket);
+    this.sockets = this.sockets.set(channel, newSockets);
+    const newUsers = this.socketsMap.has(socket.id) ? this.socketsMap.get(socket.id) : [];
+    newUsers.push(channel);
+    this.socketsMap = this.socketsMap.set(socket.id, newUsers);
+
+    if(this.debug) {
+      console.log(`Socket with id ${socket.id} added for on channel ${channel}`);
     }
   };
 
   deleteSocket(socket) {
     if(this.socketsMap.has(socket.id)) {
-      const removeFrom = this.socketsMap.get(socket.id);
-      const newSockets = this.sockets.get(removeFrom).filter((item) => item.id !== socket.id);
-      if(this.debug) {
-        console.log(`Socket with id ${socket.id} removed for user #${this.socketsMap.get(socket.id)}`);
-      }
-      this.sockets = this.sockets.set(removeFrom, newSockets);
+      const removeFromArray = this.socketsMap.get(socket.id);
+      let newSockets;
+      removeFromArray.map(removeFrom => {
+        newSockets = this.sockets.get(removeFrom).filter((item) => item.id !== socket.id);
+        if(this.debug) {
+          console.log(`Socket with id ${socket.id} removed from channel ${removeFrom}`);
+        }
+        this.sockets = this.sockets.set(removeFrom, newSockets);
+      });
+
       this.socketsMap = this.socketsMap.delete(socket.id);
     }
   }
